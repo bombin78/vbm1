ae2c330fd52d27d440ac856a51ff8235f3b531ec
bombin78 <bombin78@mail.ru>
2020-04-16 00:10:17 +0300

    Страница: "Ремонт ж.д.техники: МС, АВФ, АДМ, ж.д.кранов"

---
+++ pages/remont-zdtekhniki.vue
@@ -0,0 +1,266 @@
+<template>
+
+  <section class="page">
+
+  	<h2 class="page__content-title content-title">Ремонт ж.д.техники: МС, АВФ, АДМ, ж.д.кранов</h2>
+
+    <ul class="list">
+  		<li class="list__item">
+
+  			<h3 class="list__title">
+  				Кран железнодорожный КЖС-16
+  			</h3>
+
+  			<p class="list__text">
+  				Кран железнодорожный габаритный КЖС-16 №71 2001 года выпуска,
+  				прошел капитальный ремонт в 2015 году. Полностью укомплектован
+  				и подготовлен к работе.
+  			</p>
+
+  			<p class="list__text">
+  				После ремонта приобретен компанией ОАО "ТРАНСЭЛЕКТРОМОНТАЖ".
+  			</p>
+
+  			<table class="list__table-photo table-photo blueimp-links">
+  				<tbody>
+  				<tr class="table-photo__row">
+  					<td class="table-photo__cell">
+  						<a class="table-photo__cell-link" href="/images/repair-railway/big/kzs_1.jpg" title="Кран КЖС16 № 71 - фото 1">
+  							<img class="table-photo__cell-img" alt="1. Кран КЖС16 № 71" src="/images/repair-railway/kzs_1.jpg">
+  						</a>
+  					</td>
+  					<td class="table-photo__cell">
+  						<a class="table-photo__cell-link" href="/images/repair-railway/big/kzs_2.jpg" title="Кран КЖС16 № 71 - фото 2">
+  							<img class="table-photo__cell-img" alt="2. Кран КЖС16 № 71" src="/images/repair-railway/kzs_2.jpg">
+  						</a>
+  					</td>
+  				</tr>
+  				</tbody>
+  			</table>
+
+  			<table class="list__table-description table-description">
+  				<caption class="table-description__caption">
+  					Информация о техническом состоянии
+  				</caption>
+  				<thead class="table-description__head">
+  				<tr class="table-description__head-row">
+  					<td class="table-description__head-question">Вопрос</td>
+  					<td class="table-description__head-answer">Ответ</td>
+  				</tr>
+  				</thead>
+  				<tbody class="table-description__body">
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Полное и сокращенное наименование (модель) крана,заводской №
+  					</td>
+  					<td class="table-description__body-value">
+  						Кран Железнодорожный Дизель-Электрический
+  						Строительно-Монтажный КЖС-16 (Габаритный)
+  						№ 71
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Грузоподъемность
+  					</td>
+  					<td class="table-description__body-value">
+  						16 тонн
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Год и дата выпуска
+  					</td>
+  					<td class="table-description__body-value">
+  						2001 год
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Изготовитель
+  					</td>
+  					<td class="table-description__body-value">
+  						ОАО «Кировский машзавод 1 МАЯ»
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Стрела
+  					</td>
+  					<td class="table-description__body-value">
+  						15 метров и с грузовым крюком
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						- ДГУ (Дизель, генератор),<br>
+  						- изготовитель,<br>
+  						- тип, мощность квт (л/сил),<br>
+  						- наработка м/ч
+  					</td>
+  					<td class="table-description__body-value">
+  						Дизель ЯМЗ 238 М2  ( г. Ярославль)<br>
+  						Генератор БГ-100 (ООО «Баранчинский ЭМЗ»)<br>
+  						- мощность 176 квт (240 л/с);<br>
+  						- наработка после КР 30 мото-часов
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Комплектность
+  					</td>
+  					<td class="table-description__body-value">
+  						Полностью укомплектован штатными узлами и агрегатами.
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Период эксплуатации.<br>
+  						Постановка на учет.<br>
+  						Снятие с учёта
+  					</td>
+  					<td class="table-description__body-value">
+  						Эксплуатация с 22.05.2001 г. по 28.10.2009 г<br>
+  						Зарегистрирован 22.05.2001 г<br>
+  						Снят с учета 28.05.2013 гв
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						% износа крана
+  						до начала планового ремонта
+  					</td>
+  					<td class="table-description__body-value">
+  						До КР, в течение 8-ти лет
+  						Кран отработал всего 4135,0 мото-часов
+  						Кран до проведённого нами капитального ремонта сохранил 85% ресурса назначенного срока службы.
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Техническое состоянии крана
+  					</td>
+  					<td class="table-description__body-value">
+  						Исправен, испытан в заводских условиях тарированными грузами, готов к эксплуатации под полной нагрузкой.
+  						Предоставляется ГАРАНТИЯ качества – 6 месяцев.
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Даты последних: ТР, КР, КВР
+  					</td>
+  					<td class="table-description__body-value">
+  						Проведен КР 20.03.2015 г
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Наличие аккумуляторных батарей:
+  					</td>
+  					<td class="table-description__body-value">
+  						В наличии. Не эксплуатировались.
+  						Установлены во время КР
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Замеры колесные пары :
+  					</td>
+  					<td class="table-description__body-value">
+  						Без износа. Прошли полное техническое освидетельствованы в ОАО «РЖД» в процессе КР
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Наличие тех. паспорта (формуляра), перечень имеющейся технической документации
+  					</td>
+  					<td class="table-description__body-value">
+  						Паспорт железнодорожного крана 35-000000-000 ПС<br>
+  						Инструкция по эксплуатации.<br>
+  						- Инструкция по приведению в транспортное положение;<br>
+  						- Паспорта колёсных пар, дизель-генератора;<br>
+  						- комплект иной технической, эксплуатационной и ремонтной документации.
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Данные о регистрации.
+  					</td>
+  					<td class="table-description__body-value">
+  						Кран снят с учёта 28. 05. 2013 года, с отметкой в паспорте и после проведённого КР 20.03.2015 года кран не эксплуатировался
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Наличие действующей экспертизы промышленной безопасности.
+  					</td>
+  					<td class="table-description__body-value">
+  						Отсутствует
+  					</td>
+  				</tr>
+  				<tr class="table-description__body-row">
+  					<td class="table-description__body-label">
+  						Продление срока службы
+  					</td>
+  					<td class="table-description__body-value">
+  						Срок службы крана продлен до 04.2026 года
+  					</td>
+  				</tr>
+  				</tbody>
+  			</table>
+
+  			<p class="list__text">
+  				Кран железнодорожный дизельэлектрический строительно-монтажный КЖС-16
+  				предназначен для выполнения комплекса строительно-монтажных и
+  				погрузочно-разгрузочных работ при электрификации железных дорог
+  				колеи 1520 мм с возможностью работы на двухпутных участках без
+  				движения по параллельному пути.
+  				Унифицированный многомоторный дизельэлектрический привод на переменном
+  				токе напряжением 380 В, частотой 50 Гц и мощностью 100 кВт обеспечивает
+  				совмещение всех необходимых операций. Предусмотрена возможность работы
+  				крана с питанием от внешнего источника тока при помощи гибкого кабеля.
+  				Быстросъемное устройство позволяет производить погрузочно-разгрузочные
+  				работы под обесточенным контактным проводом при расположении стрелы
+  				поперек пути.
+  			</p>
+
+  		</li>
+  	</ul>
+
+  </section>
+
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Ремонт ж.д.техники: МС, АВФ, АДМ, ж.д.кранов',
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/utils/_variables.scss';
+@import '@/assets/scss/utils/_mixins.scss';
+
+.page {
+	@include main-content();
+}
+
+.list {
+  margin-top : 20px;
+
+	&__title {
+		font-size : $fontSize + 2px;
+		font-weight: bold;
+	}
+
+    &__text {
+      padding     : 10px 0;
+
+      line-height : 1.3;
+      text-align  : justify;
+      text-indent : 25px;
+    }
+}
+</style>
