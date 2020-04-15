53220e4276fed0f3f6d023e037ab2201b7928e8c
bombin78 <bombin78@mail.ru>
2020-04-15 22:27:11 +0300

    Правка страницы контакты

--- pages/kontakty.vue
+++ pages/kontakty.vue
@@ -1,60 +1,85 @@
 <template>
-    <section class="page">
+  <section class="page">
 
-    	<h2 class="page__content-title content-title">Контакты и реквизиты</h2>
+    <h2 class="page__content-title content-title">Контакты</h2>
 
-    	<div class="page__basic basic-info">
+    <h3 class="page__title">Общество с ограниченной ответственностью «Вибробурмашина»</h3>
 
-    		<h3 class="basic-info__title">Контакты</h3>
+    <dl class="page__company-name dtl">
+        <dt class="dtl__l">Сокращенное название:</dt>
+        <dd class="dtl__v"><strong>ООО «ВИБРОБУРМАШ»</strong></dd>
+    </dl>
 
-    		<div class="basic-info__location location-map">
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">Генеральный директор:</dt>
+        <dd class="dtl__v">Аверин Михаил Борисович</dd>
+    </dl>
 
-    			<p class="location-map__text">Расположение на карте:</p>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">Главный бухгалтер:</dt>
+        <dd class="dtl__v">Игнатьева Ирина Владимировна</dd>
+    </dl>
 
-    			<div class="location-map__img">
-    				<!-- Расположение компании на яндекс карте -->
-    				<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Azx4XsME5usvRafl7TBnSG1LV7h32iYB6&amp;width=385&amp;height=270&amp;lang=ru_RU&amp;scroll=true"></script>
-    			</div>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">Юр.адрес:</dt>
+        <dd class="dtl__v">171075, Тверская обл., Бологовский район, г.Бологое, ул.Кирпичная, д.1А., оф.1.</dd>
+    </dl>
 
-    		</div>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">Факт.адрес:</dt>
+        <dd class="dtl__v">171075, Тверская обл., Бологовский район, г.Бологое, ул.Кирпичная, д.1А., оф.1.</dd>
+    </dl>
 
-    		<div class="basic-info__top top-managers">
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">ИНН:</dt>
+        <dd class="dtl__v">6908018170</dd>
+    </dl>
 
-    			<p class="top-managers__block">
-            <span v-html="contactPerson1"></span>
-          </p>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">ОГРН:</dt>
+        <dd class="dtl__v">1196952001180</dd>
+    </dl>
 
-    			<p class="top-managers__block">
-            <span v-html="contactPerson2"></span>
-          </p>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">Тел:</dt>
+        <dd class="dtl__v">{{numPhone}}</dd>
+    </dl>
 
-    			<p class="top-managers__block">
-    				{{postCode}}<br>
-            <span v-html="location"></span><br>
-    				{{numPhone1}}<br>
-            {{numPhone2}}
-    			</p>
+    <dl class="page__dtl dtl">
+        <dt class="dtl__l">E-mail:</dt>
+        <dd class="dtl__v">
+            <a class="email" :href="`mailto:${eMail}`">
+                {{eMail}}
+            </a>
+        </dd>
+    </dl>
 
-    			<p class="top-managers__block">
-    				e-mail:
-    				<a class="top-managers__mail" :href="eMail">{{eMail}}</a>
-    			</p>
+    <h3 class="page__title">Расположение на карте</h3>
 
-    		</div>
+    <div class="page__map">
+        <!-- Расположение компании на яндекс карте -->
+        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Azx4XsME5usvRafl7TBnSG1LV7h32iYB6&amp;width=620&amp;height=280&amp;lang=ru_RU&amp;scroll=true"></script>
+    </div>
 
-    	</div>
+    <div class="schedule">
 
-    	<div class="page__company company-details">
+        <h3 class="schedule__title">Время работы</h3>
 
-    		<h3 class="company-details__title">Реквизиты</h3>
+        <div class="schedule__info">
+            <dl class="schedule__dtl dtl">
+                <dt class="dtl__l">пн-пт:</dt>
+                <dd class="dtl__v">c 08:00 до 17:00</dd>
+            </dl>
 
-    		<p class="company-details__info">
-          <span v-html="companyDetails"></span>
-        </p>
+            <dl class="schedule__dtl dtl">
+                <dt class="dtl__l">сб-вс:</dt>
+                <dd class="dtl__v">выходной</dd>
+            </dl>
+        </div>
 
-    	</div>
+    </div>
 
-    </section>
+  </section>
 
 </template>
 
@@ -65,22 +90,8 @@ export default {
   },
   data() {
     return {
-      nameCompany: 'ООО "ВИБРОБУРМАШ"',
-      contactPerson1: '<b>Генеральный директор</b><br>Аверин Михаил Борисович',
-      contactPerson2: '<b>Главный бухгалтер</b><br>Игнатьева Ирина Владимировна',
-      numPhone1: 'Тел.: +7 (920) 151-65-98',
-      numPhone2: '',
+      numPhone: 'Тел.: +7 (920) 151-65-98',
       eMail: 'info@vbm1.ru',
-      postCode: '171076',
-      location: 'Тверская обл., г.Бологое,<br>ул.Кирпичная, д.1А., офис.1',
-      companyDetails: `ООО «ВИБРОБУРМАШ»<br>
-				ИНН 6908018170; КПП 690801001<br>
-				Юр.адрес: 1171075, Тверская обл., Бологовский район, г.Бологое, ул.Кирпичная, д.1А., оф.1.<br>
-				Факт.адрес: 171075, Тверская обл., Бологовский район, г Бологое, ул.Кирпичная, д.1А., оф.1.<br>
-				Р/с 40702810763000009104,к/с 30101810700000000679, БИК 042809679<br>
-				В ТВЕРСКОM ОТДЕЛЕНИИ N8607 ПАО СБЕРБАНК<br>
-				ОГРН 1196952001180; ОКПО 35737501; ОКОГУ 4210014; ОКАТО 28408000000;<br>
-				ОКТМО 28608101001; ОКОПФ 12300; ОКВЭД 28.92; 77.39.12; 77.12; 77.11; 30.20`,
     };
   },
 }
@@ -92,72 +103,83 @@ export default {
 
 .page {
 	@include main-content();
-}
-
-.basic-info {
-	display : inline-block;
 
 	&__title {
-		margin-top  : 5px;
-
-		font-size   : $fontSize + 2px;
-		font-weight : bold;
+		margin-top: 20px;
+		font-size: $fontSize + 2px;
+		font-weight: bold;
 	}
 
-	&__location.location-map,
-	&__top.top-managers {
-		float      : left;
-		margin-top : 15px;
-	}
-}
+	&__company-name.dtl,
+	&__dtl.dtl {
+		display: flex;
+		margin-top: 8px;
 
-.location-map {
-	width : 413px;
-	&__text {
-	}
-	&__img {
-		margin-top : 5px;
-		height     : 270px;
-		width      : 385px;
-
-		border     : 2px solid $lightGray;
-	}
-}
-
-.top-managers {
-	width : 206px;
-
-	&__block {
-		margin-top : 20px;
-
-		&:first-child {
-			margin-top : 22px;
+		.dtl {
+			&__l {
+				margin-right: 5px;
+			}
 		}
 	}
 
-	&__mail {
-		color : $darkBlue;
-		&:hover {
-			text-decoration : underline;
+	&__company-name.dtl {
+		margin-top: 15px;
+
+		.dtl {
+			&__v {
+				font-weight: bold;
+			}
 		}
+	}
+
+	&__map {
+		margin-top: 5px;
+		height: 280px;
+		width: 620px;
+		border: 2px solid $lightGray;
 	}
 }
 
-.company-details {
-	margin-top : 20px;
+.email {
+	color: $darkBlue;
 
-	border-top: 1px solid $middleGray;
+	&:hover {
+		text-decoration: underline;
+	}
+}
+
+.schedule {
+	display: flex;
+	margin: {
+		top: 25px;
+		right: 20px;
+		bottom: 15px;
+	}
 
 	&__title {
-		margin-top : 5px;
-
-		font-size   : $fontSize + 2px;
-		font-weight : bold;
+		margin-right: 20px;
+		font-size: $fontSize + 2px;
+		font-weight: bold;
 	}
 
 	&__info {
-		margin-top  : 8px;
-		line-height : 1.5;
+		border-left: 2px solid $lightGray;
+    	padding-left: 20px;
+	}
+
+	&__dtl.dtl {
+		display: flex;
+		margin-top: 8px;
+
+		&:nth-of-type(1) {
+			margin-top: 0;
+		}
+
+		.dtl {
+			&__l {
+				margin-right: 5px;
+			}
+		}
 	}
 }
 
